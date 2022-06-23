7a04545a43e4eb5e56a69ef4aec6b1dac082517c
yashas2222 <104160215+yashas2222@users.noreply.github.com>
2022-06-23 12:53:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ ip = st.text_input("Enter your message:")
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])  #prints the output as spam or ham  
-
+          
   
                        
   
